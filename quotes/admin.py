e7a25de8bc789bfd0a04266816f18615a2810ddc
Amper <andrey.maliutin@gmail.com>
2025-07-02 20:48:04 +0300

    работает добавление цитат

--- quotes/admin.py
+++ quotes/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import Source, Quote
+from .models import Quote
 
-admin.site.register(Source)
 admin.site.register(Quote)
--- quotes/models.py
+++ quotes/models.py
@@ -1,16 +1,9 @@
 from django.db import models
 
 
-class Source(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
-
-
 class Quote(models.Model):
     text = models.TextField(unique=True)
-    source = models.ForeignKey(Source, on_delete=models.CASCADE)
+    source = models.CharField(max_length=255)
     weight = models.PositiveIntegerField(default=1)
     views = models.PositiveIntegerField(default=0)
     likes = models.PositiveIntegerField(default=0)
